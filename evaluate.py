68d73bcb2242dfea7b3360eb7bb2b1d064a55e04
Junkyung Kim <junkyungkim@snowboard.devices.brown.edu>
2019-06-28 14:03:06 -0400

    ,

--- evaluate.py
+++ evaluate.py
@@ -41,7 +41,7 @@ do_thinning = True
 
 bsds_or_multicue = 'bsds' #'multicue-boundaries' #'bsds'
 bsds_path = '/media/data_cifs/pytorch_projects/datasets'
-pred_path = '/media/data_cifs/cluster_projects/refactor_gammanet/bsds_for_jk/100_aug_npy'
+pred_path = '/media/data_cifs/cluster_projects/refactor_gammanet/neurips_data/bsds_for_jk/100_hed_npy/test'
 val_test = 'test'
 suffix_ext = '.npy'
 thresholds = 100
