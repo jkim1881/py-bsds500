397591f574a294f039bf11fe40646eaa77e07097
Junkyung Kim <junkyungkim@snowboard.devices.brown.edu>
2019-07-08 10:23:11 -0400

    .

--- evaluate.py
+++ evaluate.py
@@ -31,7 +31,7 @@ from skimage.io import imread
 
 zero_as_edges = True
 do_nms = False
-do_thinning = True
+do_thinning = False
 # bsds_or_multicue = 'multicue-boundaries' #'multicue-boundaries' #'bsds'
 # bsds_path = '/media/data_cifs/pytorch_projects/datasets'# '/media/data_cifs/pytorch_projects/datasets'
 # pred_path = '/media/data_cifs/pytorch_projects/MB_model_out_001' #'/media/data_cifs/pytorch_projects/MB_model_out_001' # '/media/data_cifs/pytorch_projects/model_out_001'
@@ -42,10 +42,10 @@ do_thinning = True
 bsds_or_multicue = 'bsds' #'multicue-boundaries' #'bsds'
 bsds_path = '/media/data_cifs/pytorch_projects/datasets'
 pred_path = '/media/data_cifs/cluster_projects/refactor_gammanet/neurips_data/bsds_for_jk/100_hed_npy'
-pred_path = '/media/data_cifs/pytorch_projects/model_out' #BDCN full
+# pred_path = '/media/data_cifs/pytorch_projects/model_out' #BDCN full
 val_test = 'test'
 suffix_ext = '.npy'
-suffix_ext = '.jpg'
+# suffix_ext = '.jpg'
 thresholds = 100
 
 
