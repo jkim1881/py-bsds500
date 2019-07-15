2ee7168ae83a6d9745e11eb2a5abd2745ae4e54c
Junkyung Kim <junkyungkim@Snowboard.local>
2019-07-15 12:00:18 -0400

    .

--- evaluate.py
+++ evaluate.py
@@ -30,7 +30,7 @@ from skimage.io import imread
 # thresholds = thresholds.strip()
 
 zero_as_edges = True
-do_nms = True
+do_nms = False
 do_thinning = False
 # bsds_or_multicue = 'multicue-boundaries' #'multicue-boundaries' #'bsds'
 # bsds_path = '/media/data_cifs/pytorch_projects/datasets'# '/media/data_cifs/pytorch_projects/datasets'
